0f375f1020ffc2f7c0ebfa543928f055f7b83512
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-06-14 10:58:13 +0200

    added code about console.log() and more information

---
+++ 04-outputToScreen/main.js
@@ -0,0 +1,29 @@
+// using window.alert()
+// ---------------------------------
+// alert isn't good, no body is using it now
+// also it stops every thing comming after it
+// window.alert("this is a message from window.alert()");
+
+
+// using document.write()
+// --------------------------------
+document.write("this is a message from document.write()");
+
+
+// using console.log()
+console.log("message from console.log()");
+
+// using %c directive to make styles
+console.log("styled message %cfrom console.log()\
+            %c this one is different from the previous",
+             "color:green; font-size:20px;", 
+             "color:red; font-size:40px;");
+
+
+// using console.erorr()
+console.error("error message");
+
+// using console.table()
+console.table(["boody", "ahmed", "hamdy", "hassan", "nasr"]);
+
+
