a5d16fd7dd5674a9cfa6d808aef88706855e9763
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-06-15 12:00:55 +0200

    some code about datatypes in JS

---
+++ 05-dataTypes/main.js
@@ -0,0 +1,25 @@
+// using typeof operator
+// typeof data ==== typeof(data)
+
+// string
+console.log(typeof "ahmed hassan"); // string
+console.log("ahmed hassan is " + typeof("ahmed hassan"));
+
+// number
+console.log(32 + " is " + typeof(32));
+console.log(32.23 + " is " + typeof(32.23));
+
+
+// array -> object
+console.log(typeof([32, 211, 465, "hassan", "koky"]));
+
+
+// object ->real
+console.log(typeof({name: "boody", 
+                    age: 32, 
+                    location: "helwan"}));
+
+
+// boolean
+console.log(typeof true)
+console.log(typeof false)
