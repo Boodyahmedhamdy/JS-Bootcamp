d58e60ee44e8d2c5c9439aa0bed59b60808edc4d
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-06-16 13:17:07 +0200

    var vs let vs const

---
+++ 06-variables/main.js
@@ -0,0 +1,37 @@
+// declare variable 
+var _name = "ahmed";
+console.log(_name)
+
+// variables with ID in html
+console.log(hello)
+hello.innerHTML = "another thing";
+
+// js allow $ in variable name
+var $another_Name = "boody";
+console.log($another_Name);
+
+
+// var -- everything is added to window
+var a = 32;
+var a = 10;
+console.log(a)
+
+
+// let -- can't redeclare with let
+let b = 8;
+// let b = 21; // -> you get error .. this declared before
+b = 21; // this works fine
+console.log(b)
+
+
+// const
+const c = 1;
+// c = 23; can't be changed
+console.log(c)
+
+
+
+
+
+
+
