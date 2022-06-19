fa068c1ed61bb9f6f83f28f737d09e58692fdfb3
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-06-19 11:27:01 +0200

    all aritmitic operators

---
+++ 08-arthmiticOperators/main.js
@@ -0,0 +1,33 @@
+// +
+console.log(12 + 2);
+console.log(12 + "ahmed");
+console.log("ahmed" + 10);
+
+
+// -
+console.log(12 - 2);
+console.log(12 - "ahmed"); // NaN --> not a number
+console.log("ahmed" - 10); // NaN --> not a number
+// something strang
+console.log(typeof NaN) // --> number
+
+
+// *
+console.log(12 * 2);
+console.log(12 * "ahmed");// NaN
+console.log("ahmed" * 10);// NaN
+
+// /
+console.log(12 / 2);
+console.log(12 / "ahmed"); // NaN
+console.log("ahmed" / 10); // NaN
+
+
+// ** exponentialtion
+console.log(3 ** 2);
+console.log(3 ** 4);
+
+
+
+
+
