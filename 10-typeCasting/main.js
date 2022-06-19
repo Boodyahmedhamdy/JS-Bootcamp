380372954190caf15962ca02db056fc4fec8e816
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-06-19 12:08:30 +0200

    casting in js

---
+++ 10-typeCasting/main.js
@@ -0,0 +1,20 @@
+function separate(title = "") {
+    console.log(`--------------${title}------------------`);
+}
+
+let numberInString = "10"
+let normalNumber = 20;
+let boolean = false
+
+separate("string and number addition")
+console.log(numberInString + normalNumber)
+console.log(typeof (numberInString + normalNumber))
+
+
+separate("string and number subtaction")
+console.log(numberInString - normalNumber)
+console.log(typeof (numberInString - normalNumber))
+
+
+
+
