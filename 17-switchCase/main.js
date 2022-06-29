c62dcac08296f7a29096c515885e369e4aac4220
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-06-29 10:50:19 +0200

    simple example for switch case statments

---
+++ 17-switchCase/main.js
@@ -0,0 +1,38 @@
+let day = 9
+
+
+// matching in switch using === -> the same type and value
+switch(day) {
+    case 0:
+        console.log(`day is ${day} -> saturday`)
+        break;
+    
+    case 1:
+        console.log(`day is ${day} -> sunday`)
+        break;
+
+    case 2:
+        console.log(`day is ${day} -> monday`)
+        break;
+
+    case 3:
+        console.log(`day is ${day} -> tusday`)
+        break;
+
+    case 4:
+        console.log(`day is ${day} -> wendsday`)
+        break;
+
+    case 5:
+        console.log(`day is ${day} -> thursday`)
+        break;
+    
+    case 6:
+        console.log(`day is ${day} -> friday`)
+        break;
+    
+    default:
+        console.log("wrong number value")
+        break;
+}
+
