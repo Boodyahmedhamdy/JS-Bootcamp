4ca6d122561780a43bc86f8af88114a050c54451
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-07-05 10:18:31 +0200

    challenge -- not solved

---
+++ 21-arraysChallenge/main.js
@@ -0,0 +1,16 @@
+let zero = 0
+
+let counter = 3
+
+let myArray = ["Ahmed", "Mazero", "Elham", "Osama", "Gamal", "Ameer"]
+
+
+console.log(myArray.slice(zero, 4).reverse()) // ["Osama", "Elham", "Mazero", "Ahmed"]
+
+
+console.log(myArray.slice(myArray.indexOf("Mazero"), counter).reverse()) // ["Elham", "Mazero"]
+
+
+console.log(myArray[1].slice(2)) // Elzero
+
+console.log() // "rO"
