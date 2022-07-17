efd6c41dfba010822f1fc3b693322b6860a47bea
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-07-17 10:15:44 +0200

    arrow function with examples

---
+++ 29-arrowFunction/main.js
@@ -0,0 +1,22 @@
+// regular function
+function regularFunction() {
+    // some code
+}
+
+// arrow function
+// no parameters
+
+let arrowFunction = () => {
+
+}
+
+// with parameters
+let sumArrowFunction = (n1, n2) => {
+    return n1 + n2
+}
+
+let sumArrowFunction2 = (n1, n2) => n1 + n2
+
+console.log(sumArrowFunction(1, 3))
+console.log(sumArrowFunction2(1, 3))
+ 
