c38db947c8ce77879d5d5c62cf28403a696f22b6
unknown <abdelrahmanahmedhamdy99@gmail.com>
2022-08-02 18:10:55 +0200

    nested objects

---
+++ 37-object-2/main.js
@@ -0,0 +1,16 @@
+// nested objects
+
+let user = {
+    name: "boody", 
+    age: 10, 
+    isStudent: true,
+    car: {
+        name: "BMW", 
+        modelYear: 1999, 
+        color: "black"
+    }, 
+
+}
+
+
+console.log(user)
